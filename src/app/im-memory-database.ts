2a32399ac44c1ea74bbb222d5982c997b9cd83ff
Douglas da Silva Santos <douglas.dasilvasantos@equifax.com>
2019-08-14 13:12:13 +0100

    add web-api-inmemory and service layer

---
+++ src/app/im-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Category } from "./pages/categories/shared/category.model";
+
+export class InMemoryDatabase implements InMemoryDbService{
+    createDb(){
+        const categories: Category[] = [
+            {  id:1, name: "Moradia", description: "Pagamentos de contas da casa" },
+            {  id:2, name: "Saude", description: "Plano de Saude e remedios" },
+            {  id:3, name: "Lazer", description: "Cinema, parques, praia, etc" },
+            {  id:4, name: "Salario", description: "Recebimento de salario" },
+            {  id:5, name: "Freelas", description: "Trabalhos como freelancer" },
+        ];
+
+    return { categories }    
+    }
+}
